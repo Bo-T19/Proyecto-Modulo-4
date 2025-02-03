336fd9c59d6168bcf7c34b892528e0773ab467ee
Santiago Botero Segura <santiago.b.segura@gmail.com>
2025-02-02 19:32:33 -0500

    Users page finished

--- src/react-components/UsersPage.tsx
+++ src/react-components/UsersPage.tsx
@@ -27,8 +27,8 @@ interface Props {
 export function UsersPage(props: Props) {
 
     //States
-    const [isLoading, setIsLoading] = React.useState(true);
     const [users, setUsers] = React.useState<User[]>(props.usersManager.list)
+    const [showUserForm, setShowUserForm] = React.useState(false);
 
     props.usersManager.onUserCreated = () => {
         setUsers([...props.usersManager.list])
@@ -38,11 +38,31 @@ export function UsersPage(props: Props) {
         setUsers([...props.usersManager.list])
     }
 
+    //Show or close the New Project Form
+    const handleCloseForm = () => {
+        setShowUserForm(false);
+    };
+
+
+
+    const onNewUserClick = () => {
+        setShowUserForm(true);
+    }
+
+    React.useEffect(() => {
+        if (showUserForm) {
+            const modal = document.getElementById("new-user-modal");
+            if (modal && modal instanceof HTMLDialogElement) {
+                modal.showModal();
+            }
+        }
+    }, [showUserForm]);
+
+
     //Firebase
     const projectsCollection = getCollection<IUser>("/users")
 
     const getFirestoreUsers = async () => {
-        setIsLoading(true);
 
         const firebaseUsers = await Firestore.getDocs(projectsCollection)
         for (const doc of firebaseUsers.docs) {
@@ -61,7 +81,7 @@ export function UsersPage(props: Props) {
 
         setUsers([...props.usersManager.list]);
         console.log(users)
-        setIsLoading(false);
+
     }
 
     React.useEffect(() => {
@@ -81,37 +101,19 @@ export function UsersPage(props: Props) {
                 Phone: user.phoneNumber
             }
         }));
-    
+
         const table = document.querySelector("bim-table");
         if (table) {
             table.data = formattedData;
         }
-    
+
     }, [users]);
 
     //BIM Table
     const userTable = BUI.Component.create<BUI.Table>(() => {
-        const onTableCreated = (element?: Element) => {
-            if (!element) return;
-            const table = element as BUI.Table;
-            const formattedData = users.map(user =>
-            ({
-                data: {
-                    Name: user.name,
-                    Email: user.email,
-                    Company: user.company,
-                    Role: user.role,
-                    Area: user.area,
-                    Phone: user.phoneNumber
-                }
-            }))
-            table.data = formattedData
-            console.log(formattedData)
-            
-        }
 
         return BUI.html`
-            <bim-table ${BUI.ref(onTableCreated)}></bim-table>
+            <bim-table></bim-table>
           `
     })
 
@@ -126,6 +128,36 @@ export function UsersPage(props: Props) {
             `
     })
 
+    //Sidebar with the buttons for uploading, downloading and adding new users
+
+    const header = BUI.Component.create<BUI.Component>(() => {
+        return BUI.html
+            `
+        <div style="padding: 4px; display: flex; gap: 8px;">
+            <bim-button icon="line-md:arrow-align-bottom"
+                @click=${() => {
+                props.usersManager.exportToJSON()
+            }}>
+
+            </bim-button>
+
+            <bim-button icon="line-md:arrow-align-top"
+                @click=${() => {
+                props.usersManager.importFromJSON()
+            }}>
+
+            </bim-button>
+
+            <bim-button icon="line-md:account-add"
+                @click=${() => {
+                onNewUserClick()
+            }}>
+            </bim-button>
+        </div>
+        `
+    })
+
+
     //Layouts for the grid
     const gridLayout: BUI.Layouts = {
         primary: {
@@ -136,13 +168,7 @@ export function UsersPage(props: Props) {
                 /1fr 
                 `,
             elements: {
-                header: (() => {
-                    const header = document.createElement("div")
-                    header.style.backgroundColor = "#24474e"
-                    header.style.border = "none"
-                    return header
-
-                })(),
+                header,
 
                 footer: (() => {
                     const footer = document.createElement("div")
@@ -168,228 +194,8 @@ export function UsersPage(props: Props) {
 
     return (
         <div>
+            {showUserForm ? <CreateUserForm usersManager={props.usersManager} onCloseForm={handleCloseForm} /> : <></>}
             <bim-grid id="bimGrid"></bim-grid>
         </div>
     )
-
-    //
-
-    /*
-        //States
-        const [users, setUsers] = React.useState<User[]>(props.usersManager.list)
-        const [showUserForm, setShowUserForm] = React.useState(false);
-        const [isLoading, setIsLoading] = React.useState(true);
-    
-        props.usersManager.onUserCreated = () => {
-            setUsers([...props.usersManager.list])
-        }
-    
-        props.usersManager.onUserDeleted = () => {
-            setUsers([...props.usersManager.list])
-        }
-    
-    
-        //Update the user cards
-        const userCards = users.map((user) => {
-            return (
-    
-                <UserCard user={user} key={user.id} />
-            )
-        })
-    
-    
-        //Show or close the New Project Form
-        const handleCloseForm = () => {
-            setShowUserForm(false);
-        };
-    
-    
-    
-        const onNewUserClick = () => {
-            setShowUserForm(true);
-        }
-    
-        React.useEffect(() => {
-            if (showUserForm) {
-                const modal = document.getElementById("new-user-modal");
-                if (modal && modal instanceof HTMLDialogElement) {
-                    modal.showModal();
-                }
-            }
-        }, [showUserForm]);
-    
-    
-        //For the searchbox
-        const onUserSearch = (value: string) => {
-            const filteredProjects = props.usersManager.list.filter((user) => {
-                return user.name.includes(value)
-            })
-            setUsers(filteredProjects)
-        }
-    
-        //Download the projects or upload
-        const onDownloadUsersClick = () => {
-            props.usersManager.exportToJSON()
-        }
-    
-    
-        //Download the projects or upload
-        const onUploadUsersClick = () => {
-            props.usersManager.importFromJSON()
-        }
-    
-    
-        //Firebase
-        const projectsCollection = getCollection<IUser>("/users")
-    
-        const getFirestoreUsers = async () => {
-            setIsLoading(true);
-    
-            const firebaseUsers = await Firestore.getDocs(projectsCollection)
-            for (const doc of firebaseUsers.docs) {
-                const data = doc.data()
-                const user: IUser = {
-                    ...data,
-                }
-    
-                try {
-                    props.usersManager.newUser(user)
-                }
-                catch (error) {
-                }
-            }
-    
-            setIsLoading(false);
-        }
-    
-        React.useEffect(() => {
-            getFirestoreUsers()
-        }, [])
-    
-    
-    
-    
-        return (<div className="page" id="users-page" >
-            <header>
-                <h2>Users</h2>
-                <SearchBox onChange={(value) => onUserSearch(value)} typeOfSearchBox="user" />
-                <div
-                    style={{
-                        display: "flex",
-                        flexDirection: "row",
-                        alignItems: "center",
-                        gap: 20
-    
-                    }}>
-                    <button onClick={onDownloadUsersClick} id="download">
-                        <span className="material-icons-round">download</span>
-                    </button>
-                    <button onClick={onUploadUsersClick} id="upload">
-                        <span className="material-icons-round">file_upload</span>
-                    </button>
-                    <button onClick={onNewUserClick} id="new-user">
-                        <span className="material-icons-round">add</span>New User
-                    </button>
-                </div>
-            </header>
-            {showUserForm ? <CreateUserForm usersManager={props.usersManager} onCloseForm={handleCloseForm} /> : <></>}
-            {isLoading ? (
-                <p>Loading users...</p>
-            ) :
-                (users.length > 0 ?
-    
-                    (<div id="projects-list" style={{
-                        flexGrow: 1,
-                        overflowY: "auto",
-                        padding: "20px 40px",
-                        display: "flex",
-                        flexDirection: "column",
-                        gap: "30px",
-                    }}>
-                        <div>
-                            <div
-                                style={{
-                                    display: "flex",
-                                    flexDirection: "row",
-                                    alignItems: "center",
-                                    gap: "15px",
-                                    padding: "10px",
-                                    color: "white",
-                                    backgroundColor: "#017CB3",
-                                    borderRadius: "10px"
-                                }}
-                            >
-                                <div style={{ display: "flex", alignItems: "center", gap: "15px", width: "250px" }}>
-    
-                                    Name
-    
-                                </div>
-    
-                                <div
-                                    style={{
-                                        width: "300px",
-                                        overflow: "hidden",
-                                        textOverflow: "ellipsis",
-                                        whiteSpace: "nowrap",
-                                        overflowWrap: "break-word",
-                                        textAlign: "left",
-                                    }}
-                                    className="item"
-                                >
-                                    E-mail
-                                </div>
-    
-                                <div
-                                    style={{
-                                        width: "150px",
-                                        textAlign: "left",
-                                    }}
-                                    className="item"
-                                >
-                                    Phone number
-                                </div>
-    
-                                <div
-                                    style={{
-                                        width: "250px",
-                                        overflow: "hidden",
-                                        textOverflow: "ellipsis",
-                                        whiteSpace: "nowrap",
-                                        overflowWrap: "break-word",
-                                        textAlign: "left",
-                                    }}
-                                    className="item"
-                                >
-                                    Company
-                                </div>
-    
-                                <div
-                                    style={{
-                                        width: "150px",
-                                        textAlign: "left",
-                                        overflowWrap: "break-word",
-                                    }}
-                                    className="item"
-                                >
-                                    Area
-                                </div>
-    
-                                <div
-                                    style={{
-                                        width: "150px",
-                                        textAlign: "left",
-                                        overflowWrap: "break-word",
-                                    }}
-                                    className="item"
-                                >
-                                    Role
-                                </div>
-                            </div>
-                        </div>
-    
-                        {userCards}</div>) : (<p> There are no users in the platform</p>))}
-    
-        </div>
-        )
-    */
 }
